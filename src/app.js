5399f00e9d4addc67ec5c185b9b26a660183538b
MatiasDonati <matod87@hotmail.com>
2021-10-25 20:38:19 -0300

    app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-const express = require ('express');
+// ************ Require's ************
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
+
+const app = express();
+
+// ************ Middlewares - (don't touch) ************
+app.use(express.static(path.join(__dirname, '../public'))); 
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+
+const mainRouter = require('./routes/main'); // Rutas main
+const productsRouter = require('./routes/products'); // Rutas /products
+
+app.use('/', mainRouter);
+app.use('/products', productsRouter);
+
+
